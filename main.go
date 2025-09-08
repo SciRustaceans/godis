fc674b98f999051303a7c819294bde2b5ec8fed3
SciRustaceans <jaldabe@proton.me>
2025-09-08 20:22:23 +0200

    Added aof.go containing command logic

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http/cookiejar"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,6 +19,25 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		Handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+		handler(args)
+	})
 
 	// Request recieving
 
@@ -33,8 +57,33 @@ func main() {
 			fmt.Println(err)
 			return
 		}
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length >0 ")
+			continue
+		}
 
-		fmt.Println(value)
-		conn.Write([]byte("+OK\r\n"))
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		writer := NewWriter(conn)
+
+		handler, ok := Handlers[command]
+
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
+		result := handler(args)
+		writer.Write(result)
 	}
 }
